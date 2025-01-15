9a42c93842ccc8f0dc6e206ec967e927badd4946
Nicolas Hery <hery.nicolas@gmail.com>
2025-01-15 15:21:22 -0500

    Remove op type from TypeScript example

--- typescript/main.ts
+++ typescript/main.ts
@@ -1,130 +1,123 @@
 import { z } from "zod";
 
+const ObjectType = z.enum([
+  "user",
+  "group",
+]);
+
+// type ObjectType = z.infer<typeof ObjectType>;
+type ObjectType =
+  | "user"
+  | "group";
+
 const DirectoryObject = z.object({
-  type: z.string(),
+  type: ObjectType,
   id: z.string(),
   properties: z.record(z.string(), z.string()),
 });
 
 // type DirectoryObject = z.infer<typeof DirectoryObject>;
 type DirectoryObject = {
-  type: string;
+  type: ObjectType;
   id: string;
   properties: Record<string, string>;
 };
 
 const DirectoryRelation = z
   .object({
-    objectType: z.string(),
+    objectType: ObjectType,
     objectId: z.string(),
     relation: z.string(),
-    subjectType: z.string(),
+    subjectType: ObjectType,
     subjectId: z.string(),
   });
 
 // type DirectoryRelation = z.infer<typeof DirectoryRelation>;
 type DirectoryRelation = {
-  objectType: string;
+  objectType: ObjectType;
   objectId: string;
   relation: string;
-  subjectType: string;
+  subjectType: ObjectType;
   subjectId: string;
 };
 
-const OperationType = z.enum([
-  "create_object",
-  "update_object",
-  "delete_object",
-  "create_relation",
-  "delete_relation",
-]);
-
-// type OperationType = z.infer<typeof OperationType>;
-type OperationType =
-  | "create_object"
-  | "update_object"
-  | "delete_object"
-  | "create_relation"
-  | "delete_relation";
-
-const CreateObject = OperationType.enum.create_object;
-const UpdateObject = OperationType.enum.update_object;
-const DeleteObject = OperationType.enum.delete_object;
-const CreateRelation = OperationType.enum.create_relation;
-const DeleteRelation = OperationType.enum.delete_relation;
-
 const CreateObjectOperation = z.object({
-  type: z.literal(CreateObject),
   object: DirectoryObject,
 });
 
 // type CreateObjectOperation = z.infer<typeof CreateObjectOperation>;
 type CreateObjectOperation = {
-  type: "create_object";
   object: DirectoryObject;
 };
 
 const UpdateObjectOperation = z.object({
-  type: z.literal(UpdateObject),
   object: DirectoryObject,
 });
 
 // type UpdateObjectOperation = z.infer<typeof UpdateObjectOperation>;
 type UpdateObjectOperation = {
-  type: "update_object";
   object: DirectoryObject;
 };
 
 const DeleteObjectOperation = z.object({
-  type: z.literal(DeleteObject),
-  objectType: z.string(),
+  objectType: ObjectType,
   objectId: z.string(),
 });
 
 // type DeleteObjectOperation = z.infer<typeof DeleteObjectOperation>;
 type DeleteObjectOperation = {
-  type: "delete_object";
-  objectType: string;
+  objectType: ObjectType;
   objectId: string;
 };
 
 const CreateRelationOperation = z.object({
-  type: z.literal(CreateRelation),
   relation: DirectoryRelation,
 });
 
 // type CreateRelationOperation = z.infer<typeof CreateRelationOperation>;
 type CreateRelationOperation = {
-  type: "create_relation";
   relation: DirectoryRelation;
 };
 
 const DeleteRelationOperation = z.object({
-  type: z.literal(DeleteRelation),
   relation: DirectoryRelation,
 });
 
 // type DeleteRelationOperation = z.infer<typeof DeleteRelationOperation>;
 type DeleteRelationOperation = {
-  type: "delete_relation";
   relation: DirectoryRelation;
 };
 
 const Operation = z.discriminatedUnion("type", [
-  CreateObjectOperation,
-  UpdateObjectOperation,
-  DeleteObjectOperation,
-  CreateRelationOperation,
-  DeleteRelationOperation,
+  z.object({
+    type: z.literal("create_object"),
+    value: CreateObjectOperation,
+  }),
+  z.object({
+    type: z.literal("update_object"),
+    value: UpdateObjectOperation,
+  }),
+  z.object({
+    type: z.literal("delete_object"),
+    value: DeleteObjectOperation,
+  }),
+  z.object({
+    type: z.literal("create_relation"),
+    value: CreateRelationOperation,
+  }),
+  z.object({
+    type: z.literal("delete_relation"),
+    value: DeleteRelationOperation,
+  }),
 ]);
 
 // type Operation = z.infer<typeof Operation>;
 type Operation =
-  | CreateObjectOperation
-  | UpdateObjectOperation
-  | DeleteObjectOperation
-  | CreateRelationOperation
-  | DeleteRelationOperation;
+  | { type: "create_object"; value: CreateObjectOperation }
+  | { type: "update_object"; value: UpdateObjectOperation }
+  | { type: "delete_object"; value: DeleteObjectOperation }
+  | { type: "create_relation"; value: CreateRelationOperation }
+  | { type: "delete_relation"; value: DeleteRelationOperation };
 
 const BatchRequest = z.object({
   operations: z.array(Operation),
@@ -160,26 +153,28 @@ function toImportRequestRelation(rel: DirectoryRelation): string {
   ].join("\n");
 }
 
-function toImportRequest(op: Operation): string {
-  switch (op.type) {
-    case CreateObject:
+function toImportRequest(operation: Operation): string {
+  const { type, value: op } = operation;
+
+  switch (type) {
+    case "create_object":
       return [
         "op_code: set",
         toImportRequestObject(op.object),
       ].join("\n");
 
-    case UpdateObject:
+    case "update_object":
       return [
         "op_code: set",
         toImportRequestObject(op.object),
       ].join("\n");
 
-    case DeleteObject: {
+    case "delete_object": {
       // return [
       //   "op_code: delete",
       //   toImportRequestObject(op.object)
       // ].join("\n");
-      // // Type error: Property 'object' does not exist on type ...
+      // // Type error: Property 'object' does not exist on type 'DeleteObjectOperation'
       const obj: DirectoryObject = {
         type: op.objectType,
         id: op.objectId,
@@ -191,13 +186,13 @@ function toImportRequest(op: Operation): string {
       ].join("\n");
     }
 
-    case CreateRelation:
+    case "create_relation":
       return [
         "op_code: set",
         toImportRequestRelation(op.relation),
       ].join("\n");
 
-    case DeleteRelation:
+    case "delete_relation":
       return [
         "op_code: delete",
         toImportRequestRelation(op.relation),
@@ -214,7 +209,73 @@ function toImportRequests(ops: Operation[]): string {
   return ops.map(toImportRequest).join("\n\n");
 }
 
-function main() {
+const exampleOperations: Operation[] = [
+  {
+    type: "create_object",
+    value: {
+      object: {
+        type: "user",
+        id: "b478779c-5e5e-4cd7-9bf3-1405326be526",
+        properties: {
+          email: "alice@example.com",
+        },
+      },
+    },
+  },
+  {
+    type: "update_object",
+    value: {
+      object: {
+        type: "group",
+        id: "2ca6785b-a2ef-4a62-a5f6-5e2314ae59ca",
+        properties: {
+          name: "admins",
+        },
+      },
+    },
+  },
+  {
+    type: "delete_object",
+    value: {
+      objectType: "group",
+      objectId: "c9b58dd9-b4f6-4325-ba52-3d8d70857363",
+    },
+  },
+  {
+    type: "create_relation",
+    value: {
+      relation: {
+        objectType: "group",
+        objectId: "7910720c-9789-4dd3-83a4-4c65eebd82b3",
+        relation: "member",
+        subjectType: "user",
+        subjectId: "f32756fd-6a92-4034-8b86-c92cc9d9719f",
+      },
+    },
+  },
+  {
+    type: "delete_relation",
+    value: {
+      relation: {
+        objectType: "group",
+        objectId: "c3e65031-7455-45c8-acbd-59ec59d3e769",
+        relation: "member",
+        subjectType: "user",
+        subjectId: "f50fd4aa3-d632-46d6-92da-21bcc1391287",
+      },
+    },
+  },
+];
+
+function _test() {
+  const request = BatchRequest.parse({
+    operations: exampleOperations,
+  });
+
+  console.log(JSON.stringify(request, null, 2));
+}
+
+function run() {
   const decoder = new TextDecoder("utf-8");
   const bytes = Deno.readFileSync("in.json");
 
@@ -223,6 +284,10 @@ function main() {
   const result = toImportRequests(request.operations);
 
   console.log(result);
+}
+
+function main() {
+  run();
 }
 
 if (import.meta.main) {
